2d36187424c8cd1e7ea5c18becd6332bfc82ead1
rudlevsky <rudlevsky@gmail.com>
2018-09-26 15:04:18 +0300

    first solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let zerosCount = 0; //Count of all zeros
+  let allCounts = 5;
+
+  while(allCounts <= number){
+      zerosCount += Math.trunc(number / allCounts);
+      allCounts *= 5;
+  }
+
+  return zerosCount;
 }
